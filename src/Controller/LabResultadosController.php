6fa814bc86384582f459b0d0478f7369b76dc31b
Julio Castillo <jcastillore@yahoo.es>
2021-04-20 23:57:51 -0600

    agregando rutas al controller de lab resultados

--- src/Controller/LabResultadosController.php
+++ src/Controller/LabResultadosController.php
@@ -11,60 +11,19 @@ use Symfony\Component\HttpFoundation\Request;
 class LabResultadosController extends AbstractController
 {
     /**
-     * @Route("/lab/resultados", name="lab_resultados")
+     * @Route("/lab/resultados/ordenes/pendientes", name="lab_resultados_ordenes_pendientes")
      */
-    public function index(): Response
+    public function labResultadosOrdenesPendientes(): Response
     {
-        return $this->render('lab_resultados/index.html.twig', [
-            'controller_name' => 'LabResultadosController',
-        ]);
-    }
-
-    public function getOrden(Request $request) : Response{
-        $em         = $this->getDoctrine()->getConnection();
-        $clue       = ltrim($request);
-        $limit      = $request->get('page_limit');
-        $page       = ($request->get('page') - 1) * 10;
-        $id         = $request->get('id');
-        $condition  = '';
-        $template   = $this->admin->getTemplateRegistry()->getTemplate('lab_resultados');
-
+        $request = $this->container->get('request_stack')->getCurrentRequest();
+        $numeroOrden = $request->get('numeroOrden');
         $sql = "SELECT id, fecha_orden, id_paciente
                 FROM lab_orden
-                WHERE $condition = ltrim(id,'0') ILIKE '$clue%'";
-
-        $em->prepare($sql);
-        $em->execute();
-        $get_ordenes = $em->fetchAll();
-        
-        /* return $this->renderWithExtraParams($template, [
-            'action' => 'index',
-            'ordenes' => $ordenes,
-            'csrf_token' => $this->getCsrfToken('sonata.batch'),
-        ], null); */
-
-        return new Response(json_encode($get_ordenes));
-    }
-
-    public function getOrdenAction(Request $request) : Response{
-        $em             = $this->getDoctrine()->getConnection();
-        $id             = $request->get('id') ? : null;
-        $anotaciones    = null;
-        $id_orden       = null;
-        $result         = null;
-
-        $id_orden = $em->getRepository('LabOrden')->findOneBy($id);
-
-        $dql = "SELECT a
-                FROM LabOrden a
-                WHERE a.id = :id";
-        
-        $em->prepare($dql);
-        $em->execute();
-        $ordenes = $em->fetchAll();
-
-        return $ordenes;   
-
-    }
-    
+                WHERE id = $numeroOrden";
+        $stm = $this->getDoctrine()->getConnection()->prepare($sql);
+        $stm->execute();
+        $result = $stm->fetchAll();
+        return $this->render("resultados_busqueda.twig",
+                array("datos" => json_encode($result)));
+    }   
 }
